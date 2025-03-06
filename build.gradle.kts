f41de97c8dbee3ee2b637494e720250fd24947ce
Vasyl Tkachov <fromitt@gmail.com>
2025-03-06 23:23:29 +0100

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:8.9.0")
         classpath("io.deepmedia.tools:publisher:0.6.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
