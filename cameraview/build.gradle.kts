ecb5c729037afe99f2943e0f2948a4623ce2175c
Vasyl Tkachov <fromitt@gmail.com>
2025-03-07 00:03:27 +0100

    fix publish

--- cameraview/build.gradle.kts
+++ cameraview/build.gradle.kts
@@ -40,35 +40,35 @@ dependencies {
     implementation("com.otaliastudios.opengl:egloo:0.6.1")
 }
 
-publisher {
-    project.description = "A well documented, high-level Android interface that makes capturing " +
-            "pictures and videos easy, addressing all of the common issues and needs. " +
-            "Real-time filters, gestures, watermarks, frame processing, RAW, output of any size."
-    project.artifact = "cameraview"
-    project.group = "com.otaliastudios"
-    project.url = "https://github.com/natario1/CameraView"
-    project.scm = GithubScm("fromitt", "CameraView")
-    project.addLicense(License.APACHE_2_0)
-    project.addDeveloper("fromitt", "fromitt@gmail.com")
-    release.sources = Release.SOURCES_AUTO
-    release.docs = Release.DOCS_AUTO
-    release.version = "2.8.0"
-
-    directory()
-
-    sonatype {
-        auth.user = "SONATYPE_USER"
-        auth.password = "SONATYPE_PASSWORD"
-        signing.key = "SIGNING_KEY"
-        signing.password = "SIGNING_PASSWORD"
-    }
-
-    sonatype("snapshot") {
-        repository = io.deepmedia.tools.publisher.sonatype.Sonatype.OSSRH_SNAPSHOT_1
-        release.version = "latest-SNAPSHOT"
-        auth.user = "SONATYPE_USER"
-        auth.password = "SONATYPE_PASSWORD"
-        signing.key = "SIGNING_KEY"
-        signing.password = "SIGNING_PASSWORD"
-    }
-}
+//publisher {
+//    project.description = "A well documented, high-level Android interface that makes capturing " +
+//            "pictures and videos easy, addressing all of the common issues and needs. " +
+//            "Real-time filters, gestures, watermarks, frame processing, RAW, output of any size."
+//    project.artifact = "cameraview"
+//    project.group = "com.otaliastudios"
+//    project.url = "https://github.com/natario1/CameraView"
+//    project.scm = GithubScm("fromitt", "CameraView")
+//    project.addLicense(License.APACHE_2_0)
+//    project.addDeveloper("fromitt", "fromitt@gmail.com")
+//    release.sources = Release.SOURCES_AUTO
+//    release.docs = Release.DOCS_AUTO
+//    release.version = "2.8.0"
+//
+//    directory()
+//
+//    sonatype {
+//        auth.user = "SONATYPE_USER"
+//        auth.password = "SONATYPE_PASSWORD"
+//        signing.key = "SIGNING_KEY"
+//        signing.password = "SIGNING_PASSWORD"
+//    }
+//
+//    sonatype("snapshot") {
+//        repository = io.deepmedia.tools.publisher.sonatype.Sonatype.OSSRH_SNAPSHOT_1
+//        release.version = "latest-SNAPSHOT"
+//        auth.user = "SONATYPE_USER"
+//        auth.password = "SONATYPE_PASSWORD"
+//        signing.key = "SIGNING_KEY"
+//        signing.password = "SIGNING_PASSWORD"
+//    }
+//}
