7c66f92513a3e5159062e95a1be57a1f551034f9
Vasyl Tkachov <fromitt@gmail.com>
2025-03-06 18:44:50 +0100

    update gradle and compile sdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,13 @@
-
 buildscript {
-
-    extra["minSdkVersion"] = 15
-    extra["compileSdkVersion"] = 33
-    extra["targetSdkVersion"] = 33
-
     repositories {
         google()
         mavenCentral()
-        jcenter()
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.1")
-        classpath("com.otaliastudios.tools:publisher:0.3.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-
+        classpath("com.android.tools.build:gradle:8.2.1")
+        classpath("io.deepmedia.tools:publisher:0.6.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
 
@@ -23,10 +15,5 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        jcenter()
     }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(buildDir)
 }
